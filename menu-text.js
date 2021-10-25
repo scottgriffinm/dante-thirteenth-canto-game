66cf9a6e2b4720bdd68eeebe9b416b251dffafa6
Glitch (glitch-blank-website) <none>
2021-10-25 01:40:01 +0000

    🍀🕍 Checkpoint ./menu-text.js:37904035/1727 ./style.css:37904035/12

--- menu-text.js
+++ menu-text.js
@@ -1,30 +1,8 @@
 var i = 0;
 var txt = 'Nessus had not yet reached the other side\n  When we moved forward into woods unmarked\n  By any path.';
-var txt1 = 'Nessus had not yet reached the other side';
-var txt2 = '  When we moved forward into woods unmarked';
-var txt3 = '  By any path.'
+
 var speed = 50;
 
-function newTypeWriter() {
-  for (let iii=0; iii < txt1.length; i++) {
-    document.getElementById("beginningQuote").innerHTML += txt1.charAt(i);
-    setTimeout(newTypeWriter, speed);
-  }
-  
-  document.getElementById("beginningQuote").innerHTML += '\n';
-  
-  for (let iii=0; iii < txt2.length; i++) {
-    document.getElementById("beginningQuote").innerHTML += txt2.charAt(i);
-    setTimeout(newTypeWriter, speed);
-  }
-  
-  document.getElementById("beginningQuote").innerHTML += '\n';
-  
-  for (let iii=0; iii < txt3.length; i++) {
-    document.getElementById("beginningQuote").innerHTML += txt3.charAt(i);
-    setTimeout(newTypeWriter, speed);
-  }
-}
 
 
   function typeWriter() {
@@ -35,4 +13,4 @@ function newTypeWriter() {
     }
   }
 
-newTypeWriter()
+typeWriter()
