a09757246e00f34eb1ab307205fa76a5ca016731
Glitch (glitch-blank-website) <none>
2021-10-25 01:28:08 +0000

    🏜🏚 Checkpoint ./menu-text.js:37904035/1824

--- menu-text.js
+++ menu-text.js
@@ -1,21 +1,38 @@
 var i = 0;
-var txt1 = 'Nessus had not yet reached the other side\n
-
-When we moved forward into woods unmarked\n  By any path.';
+var txt = 'Nessus had not yet reached the other side\n  When we moved forward into woods unmarked\n  By any path.';
+var txt1 = 'Nessus had not yet reached the other side';
+var txt2 = '  When we moved forward into woods unmarked';
+var txt3 = '  By any path.'
 var speed = 50;
 
-function typeWriter2() {
-  var i=0;
-  if (i < )
-}
-
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("beginningQuote").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+function newTypeWriter() {
+  for (let iii=0; iii < txt1.length; i++) {
+    document.getElementById("beginningQuote").innerHTML += txt1.charAt(i);
+    setTimeout(newTypeWriter, speed);
+  }
+  
+  document.getElementById("beginningQuote").innerHTML += '\n';
+  
+  for (let iii=0; iii < txt2.length; i++) {
+    document.getElementById("beginningQuote").innerHTML += txt2.charAt(i);
+    setTimeout(newTypeWriter, speed);
+  }
+  
+  document.getElementById("beginningQuote").innerHTML += '\n';
+  
+  for (let iii=0; iii < txt3.length; i++) {
+    document.getElementById("beginningQuote").innerHTML += txt3.charAt(i);
+    setTimeout(newTypeWriter, speed);
   }
 }
 
-typeWriter()
+
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("beginningQuote").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+newTypeWriter()
