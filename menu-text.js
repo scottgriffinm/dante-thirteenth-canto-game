0f3790cf3c45a7d633ebdca5353eda3b50946c7c
Glitch (glitch-blank-website) <none>
2021-10-25 01:17:03 +0000

    🐧👨‍❤️‍💋‍👨 Checkpoint ./menu-text.js:37904035/222 ./index.html:37904035/165 ./tree.html:37904035/104 ./style.css:37904035/147

--- menu-text.js
+++ menu-text.js
@@ -1,11 +1,21 @@
 var i = 0;
-var txt = 'Nessus had not yet reached the other side\n  When we moved forwar';
+var txt1 = 'Nessus had not yet reached the other side\n
+
+When we moved forward into woods unmarked\n  By any path.';
 var speed = 50;
+
+function typeWriter2() {
+  var i=0;
+  if (i < )
+}
+
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById("beginningQuote").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
+
+typeWriter()
