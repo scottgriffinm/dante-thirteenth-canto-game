dbd980e77ac13c89d5716109ba1ba3daf3ce571a
Glitch (glitch-blank-website) <none>
2021-10-27 00:28:39 +0000

    🍨🏆 Checkpoint ./menu-text.js:37904035/760

--- menu-text.js
+++ menu-text.js
@@ -1,15 +1,22 @@
-var i = 0;
-var stringArray = ['Nessus had not yet reached the other side', 'When we moved forward into woods', 'Unmarked by any path.'];
+var iii = 0;
+var i1 = 0, i2 = 0, i3 = 0;
+var arrayOfStrings = ['Nessus had not yet reached the other side', 'When we moved forward into woods', 'Unmarked by any path.'];
 var speed = 50;
+var len1 = arrayOfStrings[0].length;
+var len2 = arrayOfStrings[1].length;
+var len3 = arrayOfStrings[2].length;
+var elementId = 'beginningQuote';
 
-
-
-  function typeWriter(tx, elementId) {
-    if (i < txt.length) {
-      document.getElementById(elementId).innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter(txt,elementId), speed);
+  function typeWriter() {
+    if (iii < len1) {
+      document.getElementById(elementId).innerHTML += arrayOfStrings[0].charAt(i1);
+      i1++;
+      iii++;
+      setTimeout(typeWriter, speed);
+    } else if (true) {
+      
     }
+    
   }
 
   
