7af0c0f401d29de5820203608862492d51c4ab8a
Glitch (glitch-blank-website) <none>
2021-10-27 01:37:34 +0000

    🚃🌾 Checkpoint ./menu-text.js:37904035/205 ./index.html:37904035/446 ./style.css:37904035/236

--- menu-text.js
+++ menu-text.js
@@ -5,24 +5,26 @@ var speed = 50;
 var len1 = arrayOfStrings[0].length;
 var len2 = arrayOfStrings[1].length;
 var len3 = arrayOfStrings[2].length;
-var elementId = 'beginningQuote';
+var elementId1 = 'line1';
+var elementId2 = 'line2';
+var elementId3 = 'line3';
 
 
   function typeWriter() {
     if (iii < len1) {
-      document.getElementById(elementId).innerHTML += arrayOfStrings[0].charAt(i1);
+      document.getElementById(elementId1).innerHTML += arrayOfStrings[0].charAt(i1);
       i1++;
       iii++;
       setTimeout(typeWriter, speed);
     } 
      else if (iii < (len1+len2)) {
-      document.getElementById(elementId).innerHTML += arrayOfStrings[1].charAt(i2);
+      document.getElementById(elementId2).innerHTML += arrayOfStrings[1].charAt(i2);
       i2++;
       iii++;
       setTimeout(typeWriter, speed);
     } 
      else if (iii < (len1+len2+len3)) {
-      document.getElementById(elementId).innerHTML += arrayOfStrings[2].charAt(i3);
+      document.getElementById(elementId3).innerHTML += arrayOfStrings[2].charAt(i3);
       i3++;
       iii++;
       setTimeout(typeWriter, speed);
