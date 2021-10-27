0854e81863e6e53f769bd409df8c931e2b1833eb
Glitch (glitch-blank-website) <none>
2021-10-27 00:16:39 +0000

    🚃🐃 Checkpoint ./menu-text.js:37904035/151

--- menu-text.js
+++ menu-text.js
@@ -1,12 +1,10 @@
 var i = 0;
 var stringArray = ['Nessus had not yet reached the other side', 'When we moved forward into woods', 'Unmarked by any path.'];
-var arrayLength = stringArray.length;
-
 var speed = 50;
 
 
 
-  function typeWriter(txt, elementId) {
+  function typeWriter(tx, elementId) {
     if (i < txt.length) {
       document.getElementById(elementId).innerHTML += txt.charAt(i);
       i++;
@@ -14,8 +12,4 @@ var speed = 50;
     }
   }
 
-for (var iii = 0; iii < arrayLength; iii++) {
-    typeWriter(stringArray[iii], 'beginningQuote');
-  
-}
   
