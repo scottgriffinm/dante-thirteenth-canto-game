85da4197b1e14ecee27892974887b6c49e204b45
Glitch (glitch-blank-website) <none>
2021-11-02 04:32:06 +0000

    🌷👸 Checkpoint ./.glitch-assets:37904035/374 ./forest.html:37904035/276 ./style.css:37904035/83 ./forest-text.js:37904035/1064

---
+++ forest-text.js
@@ -0,0 +1,36 @@
+var iii = 0;
+var i1 = 0, i2 = 0, i3 = 0;
+var arrayOfStrings = ['The leaves not green, earth0hued;', 'When we moved forward into woods', 'Unmarked by any path.'];
+var speed = 50;
+var len1 = arrayOfStrings[0].length;
+var len2 = arrayOfStrings[1].length;
+var len3 = arrayOfStrings[2].length;
+var elementId1 = 'line1';
+var elementId2 = 'line2';
+var elementId3 = 'line3';
+
+
+  function typeWriter() {
+    if (iii < len1) {
+      document.getElementById(elementId1).innerHTML += arrayOfStrings[0].charAt(i1);
+      i1++;
+      iii++;
+      setTimeout(typeWriter, speed);
+    } 
+     else if (iii < (len1+len2)) {
+      document.getElementById(elementId2).innerHTML += arrayOfStrings[1].charAt(i2);
+      i2++;
+      iii++;
+      setTimeout(typeWriter, speed);
+    } 
+     else if (iii < (len1+len2+len3)) {
+      document.getElementById(elementId3).innerHTML += arrayOfStrings[2].charAt(i3);
+      i3++;
+      iii++;
+      setTimeout(typeWriter, speed);
+    }
+    
+  }
+
+
+  typeWriter()
