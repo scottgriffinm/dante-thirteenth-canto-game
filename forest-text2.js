de13269eedd942714bf8a2f390f140fe56cb8eb3
Glitch (glitch-blank-website) <none>
2021-11-03 00:54:34 +0000

    🐜🚪 Checkpoint ./forest-text2.js:37904035/2449

--- forest-text2.js
+++ forest-text2.js
@@ -1,33 +1,31 @@
+var speed = 100;
+var line1 = document.getElementById('line1');
+var line2 = document.getElementById('line2');
+var line3 = document.getElementById('line3');
+
+var arrayOfStrings = ['The boughs not smooth, knotted and crooked-forked;', 'No fruit, but poisoned thorns. Of the wild beasts', 'Near Cecina and Corneto, that hate fields worked'];
+var len1 = arrayOfStrings[0].length; var len2 = arrayOfStrings[1].length; var len3 = arrayOfStrings[2].length;
 var iii = 0;
 var i1 = 0, i2 = 0, i3 = 0;
-var arrayOfStrings = ['Nessus had not yet reached the other side', 'When we moved forward into woods', 'Unmarked by any path.'];
-var speed = 100;
-var len1 = arrayOfStrings[0].length;
-var len2 = arrayOfStrings[1].length;
-var len3 = arrayOfStrings[2].length;
-var elementId1 = 'line1';
-var elementId2 = 'line2';
-var elementId3 = 'line3';
 
-
-  function typeWriter(arrayOfStrings) {
+function typeWriter() {
     if (iii < len1) {
-      document.getElementById(elementId1).innerHTML += arrayOfStrings[0].charAt(i1);
+      line1.innerHTML += arrayOfStrings[0].charAt(i1);
       i1++;
       iii++;
       setTimeout(typeWriter, speed);
     } 
      else if (iii < (len1+len2)) {
-      document.getElementById(elementId2).innerHTML += arrayOfStrings[1].charAt(i2);
+      line2.innerHTML += arrayOfStrings[1].charAt(i2);
       i2++;
       iii++;
       setTimeout(typeWriter, speed);
     } 
      else if (iii < (len1+len2+len3)) {
-      document.getElementById(elementId3).innerHTML += arrayOfStrings[2].charAt(i3);
+      line3.innerHTML += arrayOfStrings[2].charAt(i3);
       i3++;
       iii++;
       setTimeout(typeWriter, speed);
     }
-    
   }
+
