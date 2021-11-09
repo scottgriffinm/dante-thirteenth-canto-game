6575e13d5f182e49056ed43336c4779c89e6064f
Glitch (glitch-blank-website) <none>
2021-11-09 04:08:32 +0000

    🌈🍲 Checkpoint ./forest.html:37904035/1128 ./style.css:37904035/128 ./forest-fade.js:37904035/219

---
+++ forest-fade.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  window.setTimeout(fadein,
+                   1); //1 second wait
+}
+
+function fadein() {
+  document.getElementById('forestQuotes1').style.opacity = '1';
+  
+  
+  
+}
