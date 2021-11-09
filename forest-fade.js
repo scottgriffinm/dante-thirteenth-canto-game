bd2f762b5962d79d2de746e8b9b8fe53fada08bd
Glitch (glitch-blank-website) <none>
2021-11-09 04:20:40 +0000

    ⛱🏮 Checkpoint ./forest.html:37904035/1394 ./forest-fade.js:37904035/2701 ./style.css:37904035/435

--- forest-fade.js
+++ forest-fade.js
@@ -1,11 +1,38 @@
+var fq = document.getElementById('forestQuotes');
+
+
 window.onload = function() {
   window.setTimeout(fadein,
                    1); //1 second wait
 }
 
 function fadein() {
-  document.getElementById('forestQuotes1').style.opacity = '1';
-  
+  if (fq.innerText == 'The leaves not green, earth-hued;') && (fq.style.opacity == '0'){
+    fq.style.opacity = '1';  
+  }
+    
   
   
 }
+
+        <div id="forestQuotes1" style="position:absolute;">
+            The leaves not green, earth-hued;
+        </div>
+        <div id="forestQuotes2" style="position:absolute;">
+            The boughs not smooth, knotted and crooked-forked;<br>&emsp;No fruit, but poisoned thorns. Of the wild beasts
+          <br>&emsp;Near Cecina and Corneto, that hate fields worked
+        </div>
+        <div id="forestQuotes3" style="position:absolute;">
+          By men with plough and harrow, none infests<br>&emsp;Thickets that are as rough or dense as this.
+          <br>&emsp;Here the repellent Harpies make their nests,
+        </div>
+        <div id="forestQuotes4" style="position:absolute;">
+          Who drove the Trojans from the Strophades<br>&emsp;With dire announcements of the coming woe.
+          <br>&emsp;They have broad wings, a human neck and face,
+        </div>
+        <div id="forestQuotes5" style="position:absolute;">
+          Clawed feet, and swollen, feathered bellies; they caw<br>&emsp;Their lamentations in the eerie trees.
+        </div>
+        <div id="forestQuotes6" style="position:absolute;">
+          &emsp;On every side, I heard wailing voices grieve,<br>&emsp;Yet I could not see anyone there to wail,
+        </div>
