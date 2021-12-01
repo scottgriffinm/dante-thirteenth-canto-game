ee2de01d6d3ba3d9b92e2bbdf91b0a42157f5d15
Glitch (dante-thirteenth-canto) <none>
2021-12-01 23:08:36 +0000

    🐬📠 Checkpoint ./underText-fade.js:37904035/272 ./forest.html:37904035/165 ./style.css:37904035/107

---
+++ underText-fade.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  window.setTimeout(fadein,
+                   15000); //15 seconds - TIME UP WITH TEXT GIF WHEN READY
+}
+
+function fadein() {
+  document.getElementById('underText').style.opacity = '1';
+}
