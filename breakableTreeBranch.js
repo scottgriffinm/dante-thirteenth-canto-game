55e09a5c58b1e114818a1dc4f512bb6a4c1ea95a
Glitch (dante-thirteenth-canto) <none>
2021-12-04 04:04:57 +0000

    ⏰🌉 Checkpoint ./breakableTreeBranch.js:37904035/6265 ./style.css:37904035/5

--- breakableTreeBranch.js
+++ breakableTreeBranch.js
@@ -1,23 +1,22 @@
-let branch  = document.getElementById("treeHole");
 
-branch.onmouseover = function() {mouseOver()};
-branch.onmouseout = function() {mouseOut()};
+window.onload = function(){ 
+  var branch  = document.getElementById("treeHole");
+  branch.onclick = function() {mouseDown()};
+  branch.onmouseover = function() {mouseOver()};
+  branch.onmouseout = function() {mouseOut()};
+  function mouseOver() {
+    branch.opacity = 0.5;
+  }
+
+  function mouseOut() {
+    branch.opacity = 0;
+  }
+
+  function mouseDown() {
+    document.getElementById("treeImage").src = "https://cdn.glitch.me/043378ab-3600-4494-be86-3cd6d7062f17%2Fbrokentree%20with%20dante%20and%20virgil.png?v=1638588909585";
+    branch.remove(); 
+  }
 
 
-function mouseOver() {
-  branch.opacity = "red";
-}
+};
 
-function mouseOut() {
-  document.getElementById("demo").style.color = "black";
-}
-
-
-
-
-
-
-branch.onmousedown = function(event) {
-      document.getElementById("treeImage").src = "https://cdn.glitch.me/043378ab-3600-4494-be86-3cd6d7062f17%2Fbrokentree%20with%20dante%20and%20virgil.png?v=1638588909585";
-      branch.remove(); 
-    }
