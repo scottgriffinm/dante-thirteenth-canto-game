b5c828a53b58e7899a04bb4992e267335bc6e007
Glitch (dante-thirteenth-canto) <none>
2021-12-04 03:53:40 +0000

    👗🍳 Checkpoint ./breakableTreeBranch.js:37904035/625 ./tree.html:37904035/251 ./style.css:37904035/2

--- breakableTreeBranch.js
+++ breakableTreeBranch.js
@@ -1,7 +1,23 @@
 let branch  = document.getElementById("treeHole");
 
+branch.onmouseover = function() {mouseOver()};
+branch.onmouseout = function() {mouseOut()};
 
-    branch.onmousedown = function(event) {
-      document.getElementby
-      
+
+function mouseOver() {
+  branch.opacity = "red";
+}
+
+function mouseOut() {
+  document.getElementById("demo").style.color = "black";
+}
+
+
+
+
+
+
+branch.onmousedown = function(event) {
+      document.getElementById("treeImage").src = "https://cdn.glitch.me/043378ab-3600-4494-be86-3cd6d7062f17%2Fbrokentree%20with%20dante%20and%20virgil.png?v=1638588909585";
+      branch.remove(); 
     }
